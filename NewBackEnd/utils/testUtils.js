c9919182080ea79bca702b27f9cf4fe7a9746a85
MaximeDev <maxime.devillepoix1662@gmail.com>
2024-05-14 20:09:55 +0200

    MD- rien de spécial, besoin de commit

---
+++ NewBackEnd/utils/testUtils.js
@@ -0,0 +1,20 @@
+const mockRequest = () => {
+    const req = {};
+    req.body = jest.fn().mockReturnValue(req);
+    req.params = jest.fn().mockReturnValue(req);
+    req.headers = jest.fn().mockReturnValue(req);
+    return req;
+  };
+  
+  const mockResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+  };
+  
+  module.exports = {
+    mockRequest,
+    mockResponse,
+  };
+  
