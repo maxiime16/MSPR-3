e62d5a82ff125debbde632fa33e662c9bbf0b950
ezonnes <enzo18072003@gmail.com>
2024-05-22 14:10:46 +0200

    EDS changement script sh pour faire en sorte que le github action run la totalité des choses demandé

--- run-all-tests-installs.sh
+++ run-all-tests-installs.sh
@@ -4,14 +4,26 @@
 run_npm_commands() {
   local dir=$1
   cd "$dir" || exit
-  echo "Running npm install and test in $dir"
+  echo "Running npm install in $dir"
   npm install
-  npm test
+
+  # Check if the package.json has a test script before running npm test
+  if grep -q "\"test\"" package.json; then
+    echo "Running npm test in $dir"
+    npm test
+  else
+    echo "No test script found in $dir/package.json"
+  fi
+
   cd - || exit
 }
 
 # List of directories containing package.json
 directories=("Authentification" "BackEnd" "FrontEnd" "Messagerie")
+
+# Use nvm to set the Node.js version
+echo "Switching to Node.js version 18 using nvm"
+nvm use 18
 
 # Iterate over directories and run npm commands
 for dir in "${directories[@]}"; do
